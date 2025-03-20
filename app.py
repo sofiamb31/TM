88f3d07ab364a70b428fe98fc72810422ff5e7ec
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-03-20 10:38:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if img_file_buffer is not None:
     prediction = model.predict(data)
     print(prediction)
     if prediction[0][0]>0.5:
-      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
+      st.header('Sofia, con Probabilidad: '+str( prediction[0][0]) )
     if prediction[0][1]>0.5:
       st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
     #if prediction[0][2]>0.5:
